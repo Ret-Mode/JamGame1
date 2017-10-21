2e039098ca3419d87279c6535b6e89c7707a9ce9
Ret_Mode_Home_Win <home@ret_mode.home.win>
2017-10-21 16:30:01 +0200

    compilation batch added

---
+++ compile_debug_xamarin.bat
@@ -0,0 +1,2 @@
+set PATH="C:\Program Files (x86)\MSBuild\14.0\Bin";%PATH%
+msbuild /target:Game1 /property:Configuration=Debug /property:Platform=x86 ..\XamarinProject\Game1.sln
--- src/Game1.cs
+++ src/Game1.cs
@@ -13,47 +13,47 @@ namespace Game1
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "assets";
-
-			// NOTE (R-M): test for CSharp compiler
-			Compiler c = new Compiler("Scripts\\test.script", true);
+	    
+	    // NOTE (R-M): test for CSharp compiler
+	    Compiler c = new Compiler("Scripts\\test.script", true);
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+	    
             base.Initialize();
         }
-
+	
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+	    
             // TODO: use this.Content to load your game content here
         }
-
+	
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
-
+	
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+	    
             // TODO: Add your update logic here
-
-			base.Update(gameTime);
+	    
+	    base.Update(gameTime);
         }
-
+	
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+	    
             // TODO: Add your drawing code here
-
+	    
             base.Draw(gameTime);
         }
     }
