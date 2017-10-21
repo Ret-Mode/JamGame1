f3d2b48dc59efe34296e6599f005b7b0080c2f9b
Ret_Mode_Home_Win <home@ret_mode.home.win>
2017-10-21 14:18:32 +0200

    Script system small fix

--- src/Compiler.cs
+++ src/Compiler.cs
@@ -5,35 +5,30 @@ namespace Game1
 {
 	public class Compiler
 	{
-		CodeDomProvider CSCP;	
-		CompilerParameters CP;
 
-		public Compiler()
+
+		public Compiler(String FileName, bool InMemory)
 		{
-			CSCP = CodeDomProvider.CreateProvider("CSharp");///new CSharpCodeProvider();
+			CodeDomProvider CSCP;
+			CompilerParameters CP;
+			CSCP = CodeDomProvider.CreateProvider("CSharp");
 			CP = new System.CodeDom.Compiler.CompilerParameters
 			{
-				GenerateExecutable = false,
-				GenerateInMemory = true
+				GenerateExecutable = !InMemory,
+				GenerateInMemory = InMemory
 			};
 			CP.ReferencedAssemblies.Add("System.dll");
 
-			var results = CSCP.CompileAssemblyFromSource(CP, new string[]
-			{@" using System;
-
-    			class MyClass
-    			{
-        			public void Message(string message)
-        			{
-            			Console.Write(message);
-        			}               
-    			}"
-			});
+			///CompilerResults results = CSCP.CompileAssemblyFromSource(CP, new string[]
+			CompilerResults results = CSCP.CompileAssemblyFromFile(CP, FileName);
 			if (results.Errors.Count == 0)
 			{
-    			var myClass = results.CompiledAssembly.CreateInstance("MyClass");
+    			object myClass = results.CompiledAssembly.CreateInstance("MyClass");
 				myClass.GetType().GetMethod("Message").Invoke(myClass, new []{ "Hello World!" });
+				myClass = null;
 			}
+			CP = null;
+			CSCP = null;
 		}
 	}
 }
