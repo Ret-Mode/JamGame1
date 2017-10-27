c50718ff5e4d4d63480e4327387c03e52cbba999
Ret_Mode_Home_Win <home@ret_mode.home.win>
2017-10-27 20:21:59 +0200

    Let's try working with one project file  Seems that Xamarin have same file format as VS12 for sln and csproj.  We'll se what comes up from this

--- assets.bat
+++
@@ -1,1 +0,0 @@
-xcopy  %1\Game1\Content\bin\assets\* %2\Output\Content\ /Y /S /F
--- compile_debug_vs2012.bat
+++
@@ -1,2 +0,0 @@
-set PATH="C:\Program Files (x86)\MSBuild\14.0\Bin";%PATH%
-msbuild /target:Game1 /property:Configuration=Debug /property:Platform=x86 VisualStudio12\Game1.sln
